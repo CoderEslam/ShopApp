19c379af094b2f670bfb5a21798dee1fb40a2f5a
CoderEslam <eslamghazy600@gmail.com>
2023-09-13 19:10:04 +0200

    Apple Pay

---
+++ ShopApp/Views/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  ShopApp
+//
+//  Created by Eslam Ghazy on 13/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var cartManager = CartManager()
+    var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(productList, id: \.id) { product in
+                        ProductCard(product: product)
+                            .environmentObject(cartManager)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(Text("Sweater Shop"))
+            .toolbar {
+                NavigationLink {
+                    CartView()
+                        .environmentObject(cartManager)
+                } label: {
+                    CartButton(numberOfProducts: cartManager.products.count)
+                }
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
